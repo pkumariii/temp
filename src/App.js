ecdc08eb30eb8bec2f2ce0c1b11afdd1db97da07
pkumariii <prasannakumarikumari871@gmail.com>
2025-07-16 15:23:38 +0530

    add:Lmsc:90876 push changes formb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
 function App() {
 
   return (
-    <ComponentA />
+    <ComponentB/>
   )
 }
 
